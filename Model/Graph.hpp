e1c36d51a53004eca86625f6dc13ffb5ac6ba81c
Jac0bAnderson <jake912235@gmail.com>
2016-04-25 08:55:07 -0600

    Added code to the Graph.cpp.

--- Model/Graph.hpp
+++ Model/Graph.hpp
@@ -8,20 +8,28 @@
 
 #ifndef Graph_hpp
 #define Graph_hpp
-
+#include <set>
 #include <stdio.h>
 template <class Type>
 class Graph
 {
 private:
     static const int MAXIMUM =20;
+    bool edges [MAXIMUM] [MAXIMUM];
+    Type labels [MAXIMUM];
+    int manyVertices;
 public:
     Graph();
     ~Graph();
+    
     void addVertex(const Type& label);
     void addEdge(int source, int target);
     void removeEdge(int source, int target);
     Type& operator [] (int vertex);
+    int size() const;
+    bool isEdge(int source, int target) const;
+    std::set<int> neighbors(int vertex);
+    Type operator [] (int vertex) const;
 };
 
 #endif /* Graph_hpp */
